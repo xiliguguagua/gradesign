25d7f5a30ffec8e523b74357f750397dbe9b1581
叽歪 <72652354+xiliguguagua@users.noreply.github.com>
2023-02-01 20:19:11 +0800

    add pytorch demo

---
+++ torch_demo.py
@@ -0,0 +1,38 @@
+import torch
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+def get_args():
+    parser = argparse.ArgumentParser(description="train MSFL")
+    parser.add_argument("--N", type=int, default=100,
+                        help="user num")
+    parser.add_argument("--M", type=int, default=10,
+                        help="shuffler num")
+    parser.add_argument("--T", type=int, default=150,
+                        help="total communication round")
+    parser.add_argument("--k", type=int, default=10,
+                        help="least user num in a shuffler")
+    parser.add_argument("--local_epoch", type=int, default=2,
+                        help="epoch for each user in one communication turn")
+    parser.add_argument("--batch_size", type=int, default=5,
+                        help="batch size")
+    parser.add_argument("--shuffle_size", type=int, default=100,
+                        help="shuffle size to random choose sample for user in one step")
+    parser.add_argument("--prefetch_size", type=int, default=10,
+                        help="prefetch size")
+    parser.add_argument("--fA_coeff", type=float, default=0.001,
+                        help="zeta in fA()")
+    parser.add_argument("--fC_coeff", type=float, default=1.,
+                        help="ell in fC()")
+    parser.add_argument("--B_coeff", type=float, default=0.5,
+                        help="V in B()")
+    parser.add_argument("--DMS_rho", type=float, default=1.,
+                        help="rho in DMS-p")
+    parser.add_argument("--DMS_h", type=float, default=0.01,
+                        help="h in DMS-p")
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    pass
